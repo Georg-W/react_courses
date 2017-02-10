b806295d34b3ad42931b93e66f8c04ac10ca0bdf
Georg Westner <Georg Westner>
2017-02-10 23:56:59 +0100

    inital commit

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <h1>Hallo</h1>
+    );
+  }
+}
+
+export default CoursesPage;
